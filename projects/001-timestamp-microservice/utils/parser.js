482058309375ed24d93950d8b19d4a47dc67e6e6
FrancescoDi <>
2022-03-29 14:37:11 +0000

    dateParser refactor

--- projects/001-timestamp-microservice/utils/parser.js
+++ projects/001-timestamp-microservice/utils/parser.js
@@ -1,49 +1,55 @@
 function dateParser (date2parse) {
 
-  const dateBase = isNow(date2parse);
+  // if date2parse is null, set date = today
+  const date = (date2parse === undefined)
+    ? Date.now() : date2parse
 
-  return getDateFormat(dateBase)
-
-  // check if value is null, the default date is now()
-  function isNow (date2parse) {
-    return (date2parse === undefined) ? new Date(Date.now()) : new Date(date2parse)
-  }
-
-
-  // check if the format is date_string or timestamp or an invalid date
-  function getDateFormat (date) {
-    // correct date_string format
-    if (!isNaN(date)) {
-      return {
-        'unix-date': date.getTime(),
-        'UTC-date': date.toUTCString()
-      }
-    } else {
-      // if is not NaN, is already unix format
-      if (!isNaN(parseInt(date2parse))) {
-        return {
-          'unix-date': parseInt(date2parse),
-          'UTC-date': new Date(parseInt(date2parse)).toUTCString()
-        }
-      }
-
-      // any other option is invalid
-      return {error: "Invalid Date"}
+  /*
+  if the date_string is correct, return an obj having keys 'unix-date' and 'UTC-date', using new Date to convert the string to [object Date] and call getTime method and toUTCString method
+  */
+  if (isValidDate(date)) {
+    return {
+      'unix-date': new Date(date).getTime(),
+      'UTC-date': new Date(date).toUTCString()
     }
   }
+  /*
+  if the date is in number format, return an obj having keys 'unix-date' and 'UTC-date', using parseInt(date) to set the value of 'unix-date' (bypass the new Date creation) and creating a new [object Date] using new Date(parseInt(date)) to call toUTCString method
+  */
+  else if (isValidUnixTimestamp(date)) {
+    return {
+      'unix-date': parseInt(date),
+      'UTC-date': new Date(parseInt(date)).toUTCString()
+    }
+  }
+
+  // return error for any other string
+  return {error: "Invalid Date"}
 }
+
+
+// check if a date_string is valid
+function isValidDate (date) {
+  return (!isNaN(new Date(date))) ? true : false
+}
+
+// check if a string is in unix timestamp format
+function isValidUnixTimestamp (date) {
+  return (!isNaN(parseInt(date))) ? true : false
+}
+
 
 module.exports = {
   dateParser
 }
 
-// console.log(dateParser('2015-12-25'));
-// console.log(dateParser('1451001600000'));
-// console.log(dateParser('nice guy'));
-// console.log(dateParser());
-// console.log(dateParser('1995, 11, 17'));
-// console.log(dateParser('December 17, 1995 03:24:00'));
-
-// console.log(dateParser('9'));
-// console.log(dateParser('145'));
-// console.log(dateParser('2998'));
+console.log(dateParser('2015-12-25'));
+console.log(dateParser('1451001600000'));
+console.log(dateParser('nice guy'));
+console.log(dateParser());
+console.log(dateParser('1995, 11, 17'));
+console.log(dateParser('December 17, 1995 03:24:00'));
+console.log(dateParser('9'));
+console.log(dateParser('145'));
+console.log(dateParser('2998'));
+console.log(dateParser('30000.77'));
